00711a4d0fbe850a361a45128062cc96edce9b22
Skyler Lutz <skylerlutz@Skylers-MacBook-Pro.local>
2015-06-19 12:39:27 -0500

    controller refactor!

--- Digicalytics/Digicalytics/TimelineScreen.swift
+++ Digicalytics/Digicalytics/TimelineScreen.swift
@@ -17,157 +17,120 @@ class TimelineScreen: UIViewController, UITableViewDataSource, UITableViewDelega
         return formatter
     }()
     
+    var buttons : [Button] = []
+    
     @IBOutlet weak var tableView: UITableView!
     
     let textCellIdentifier = "sensorDataCell"
     
-    let actionValues = ["OpenDoor", "Open Cabinet", "Turn On Light", "Take Medicine"]
-    
-//    var time = ["1:58pm","","","11:49am","","10:15am","","9:59am","9:14am","","","","","","","3:11am","","","1:42am","",""]
-    
-    var time: [NSDate?] = []
-    
-    var action: [String] = []
-    
-//    var sensorAction = ["Ready for Demo","","","Start Panicking","","Feeling confident","","Arguing","Beer Fridge","","","","","","","Go to Sleep","","","Leave work","",""]
-    
-    
-    func getParseData() {
-        var query = PFQuery(className:"button")
-//        query.whereKey("type", equalTo:"Door")
-        query.orderByDescending("createdAt")
-        query.findObjectsInBackgroundWithBlock {
-            (objects: [AnyObject]?, error: NSError?) -> Void in
-            
-            if error == nil {
-                // The find succeeded.
-                println("Successfully retrieved \(objects!.count) datapoints.")
-                // Do something with the found objects
-                if let objects = objects as? [PFObject] {
-                    
-                    for i in 0 ..< objects.count {
-                        let object = objects[i]
-//                        println(object["Time"])
-
-//                        println("\(str)")
-                        
-                        var iD = object["sensorID"] as! Int
-//                        println()
-//                        var addTime = object["Time"] as! String
-                        var addAction = self.actionValues[iD]
-                        
-                        
-                        
-                        self.time.append(object.createdAt)
-                        self.action.append(addAction)
-                        
-                        // if this isn't the last index then check the time of the next object
-                        if i != objects.count - 1 {
-                            let object2 = objects[i+1]
-                            
-                            let cal = NSCalendar.currentCalendar()
-                            
-                            let comps = cal.components(NSCalendarUnit.CalendarUnitHour, fromDate: object2.createdAt!, toDate: object.createdAt!, options: nil)
-                            for j in 0..<comps.hour {
-                                self.time.append(nil)
-                                self.action.append("")
-                            }
-                        }
-                    }
-                }
-            } else {
-                // Log details of the failure
-                println("Error: \(error!) \(error!.userInfo!)")
-            }
-            
-            
-            
-            self.tableView.reloadData()
-            NSTimer.scheduledTimerWithTimeInterval(5.0, target: self, selector: "getNewParseData", userInfo: nil, repeats: true)
-        }
-    }
-    
-    func getNewParseData(){
-        
-        let query = PFQuery(className: "button")
-        query.whereKey("createdAt", greaterThan: time[0]!)
-        query.orderByDescending("createAt")
-        query.findObjectsInBackgroundWithBlock {
-            (objects: [AnyObject]?, error: NSError?) -> Void in
-            
-            if error == nil {
-                if let object = objects?.first as? PFObject {
-                    self.time.insert(object.createdAt, atIndex: 0)
-                    self.action.insert(self.actionValues[object["sensorID"] as! Int], atIndex: 0)
-                    
-                    self.tableView.insertRowsAtIndexPaths([NSIndexPath(forRow: 0, inSection: 0)], withRowAnimation: UITableViewRowAnimation.Automatic)
-                }
-            }else{
-                println("could not fetch new parse data.")
-            }
-        }
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        getParseData()
         
         tableView.delegate = self
         tableView.dataSource = self
         
-        
         var nib = UINib(nibName: "vwTblCell", bundle: nil)
         tableView.registerNib(nib, forCellReuseIdentifier: "cell")
         
-        
+        getParseData()
     }
-    
     
     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         return 1
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return time.count
+        return self.buttons.count
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell:customTableViewCell = self.tableView.dequeueReusableCellWithIdentifier("cell") as customTableViewCell
         
-        var cell:customTableViewCell = self.tableView.dequeueReusableCellWithIdentifier("cell") as! customTableViewCell
-        
-        
-//        let cell = tableView.dequeueReusableCellWithIdentifier(textCellIdentifier, forIndexPath: indexPath) as! UITableViewCell
-        
-        let row = indexPath.row
-        
-        if let date = time[row]{
-            cell.timeLbl.text = formatter.stringFromDate(date)
-        } else {
-            cell.timeLbl.text = nil
-        }
-        cell.sensorActionLbl.text = action[row]
-        
-        if time[row] == nil{
-            cell.backgroundImage.image = UIImage(named: "NoData.png")
-        }else{
-            cell.backgroundImage.image = UIImage(named: "Data.png")
-        }
+        var button = self.buttons[indexPath.row]
+        cell.timeLbl.text = self.formatter.stringFromDate(button.createdAt)
+        cell.sensorActionLbl.attributedText = makeAttributed(button.sensorID)
         
         return cell
     }
     
-    func tableView(tableView: UITableView, didDeselectRowAtIndexPath indexPath: NSIndexPath) {
-        tableView.deselectRowAtIndexPath(indexPath, animated: true)
-        
-        let row = indexPath.row
-        println("Selected \(time[row])")
-    }
-    
     //MARK: - UIButton Delegates
+    
+    func makeAttributed(old : NSString) -> NSMutableAttributedString {
+
+
+        let fontSize : CGFloat = 13.0;
+        let boldFont = UIFont.boldSystemFontOfSize(fontSize)
+        let regularFont = UIFont.systemFontOfSize(fontSize)
+        let foregroundColor = UIColor.blackColor()
+        let attrs = [NSFontAttributeName : boldFont, NSForegroundColorAttributeName : foregroundColor]
+        let subAttrs = [NSFontAttributeName : regularFont]
+
+        let range = old.rangeOfString(" ")
+        
+        let s = NSMutableAttributedString(string: old, attributes: attrs)
+        s.setAttributes(subAttrs, range: range)
+        return s
+    }
     
     @IBAction func addSensor(sender: AnyObject) {
         println("Add Button Pressed!")
     }
     
+    func getParseData() {
+        
+        var query = PFQuery(className:"button")
+        query.orderByDescending("createdAt")
+        query.findObjectsInBackgroundWithBlock {
+            (objects: [AnyObject]?, error: NSError?) -> Void in
+            if error != nil {
+                println("Error: \(error!) \(error!.userInfo!)")
+                return
+            }
+            
+            var buttons : [Button] = []
+            
+            if let objects = objects as? [PFObject] {
+                for object in objects {
+                    println(object)
+                    buttons.append(Button(sensorID: object["sensorID"] as Int, createdAt: object.createdAt))
+                }
+            }
+            self.buttons = buttons
+            self.tableView.reloadData()
+        }
+        NSTimer.scheduledTimerWithTimeInterval(5.0, target: self, selector: "getNewParseData", userInfo: nil, repeats: true)
+    }
+    func getNewParseData(){
+
+        let query = PFQuery(className: "button")
+        if let first = self.buttons.first {
+            query.whereKey("createdAt", greaterThan: first)
+        }
+        else {
+            return
+        }
+        query.orderByDescending("createAt")
+        query.findObjectsInBackgroundWithBlock {
+        (objects: [AnyObject]?, error: NSError?) -> Void in
+        
+            if error != nil {
+                println("could not fetch new parse data.")
+            }
+            for object in objects as [PFObject] {
+                if let object = objects?.first as? PFObject {
+                    self.buttons.insert(Button(sensorID: object["sensorId"] as Int, createdAt: object.createdAt), atIndex: 0)
+                    self.tableView.insertRowsAtIndexPaths([NSIndexPath(forRow: 0, inSection: 0)], withRowAnimation: UITableViewRowAnimation.Automatic)
+            }
+        }
+
+    }
+}
+}
+class Button {
+    let sensorID : String
+    let createdAt: NSDate
+    init(sensorID : Int, createdAt: NSDate) {
+        self.sensorID = ["OpenDoor", "Open Cabinet", "Turn On Light", "Take Medicine"][sensorID]
+        self.createdAt = createdAt
+    }
 }
