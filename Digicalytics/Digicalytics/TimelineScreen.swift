0ef1bb90d7ffac069f95b7d5d7021de8ecb0029c
Skyler Lutz <skylerlutz@Skylers-MacBook-Pro.local>
2015-06-19 13:10:18 -0500

    icons

--- Digicalytics/Digicalytics/TimelineScreen.swift
+++ Digicalytics/Digicalytics/TimelineScreen.swift
@@ -82,8 +82,21 @@ class TimelineScreen: UIViewController, UITableViewDataSource, UITableViewDelega
         var button = self.buttons[indexPath.row]
         cell.timeLbl.text = self.formatter.stringFromDate(button.createdAt)
         cell.sensorActionLbl.attributedText = makeAttributed(button.sensorID)
-        cell.backgroundImage.image = UIImage(named: "")
-        //  ["OpenDoor", "Open Cabinet", "Turn On Light", "Take Medicine"]
+        cell.backgroundColor = UIColor.clearColor()
+        
+        
+        var filename : String
+        switch(button.sensorIdx) {
+        case 0:
+            filename = "ic_Door"
+        case 1:
+            filename = "ic_Light"
+        case 2:
+            filename = "ic_Medicine"
+        default:
+            filename = "ic_Cabinet"
+        }
+        cell.imageView.image = UIImage(named: filename)
         return cell
     }
     
@@ -158,7 +171,6 @@ class TimelineScreen: UIViewController, UITableViewDataSource, UITableViewDelega
                     self.tableView.insertRowsAtIndexPaths([NSIndexPath(forRow: 0, inSection: 0)], withRowAnimation: UITableViewRowAnimation.Automatic)
             }
         }
-
     }
     }
 }
@@ -169,7 +181,7 @@ class Button {
     let createdAt: NSDate
     let sensorIdx: Int
     init(sensorID : Int, createdAt: NSDate) {
-        self.sensorID = ["OpenDoor", "Open Cabinet", "Turn On Light", "Take Medicine"][sensorID]
+        self.sensorID = ["OpenDoor", "Turn On Light", "Take Medicine", "Open Cabinet"][sensorID]
         self.createdAt = createdAt
         self.sensorIdx = sensorID
     }
